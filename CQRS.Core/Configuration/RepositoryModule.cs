0434dfb9df3b2f783012eb16548143101e480890
Sam Critchley <critchley.sj@gmail.com>
2012-07-31 17:36:55 +1000

    added repository autofac module (untested)

---
+++ CQRS.Core/Configuration/RepositoryModule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Autofac;
+using CQRS.Core.Infrastructure;
+using Module = Autofac.Module;
+
+namespace CQRS.Core.Configuration
+{
+    public class RepositoryModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            Type repositoryType = typeof (IRepository<>);
+            IEnumerable<Type> repositories = from x in assemblies.SelectMany(a => a.GetTypes())
+                                             from z in x.GetInterfaces()
+                                             let y = x.BaseType
+                                             where
+                                                 (y != null && y.IsGenericType &&
+                                                  repositoryType.IsAssignableFrom(y.GetGenericTypeDefinition())) ||
+                                                 (z.IsGenericType &&
+                                                  repositoryType.IsAssignableFrom(z.GetGenericTypeDefinition()))
+                                             select x;
+
+            builder.RegisterAssemblyTypes(assemblies)
+                .Where(repositories.Contains)
+                .AsImplementedInterfaces();
+
+            base.Load(builder);
+        }
+    }
+}
