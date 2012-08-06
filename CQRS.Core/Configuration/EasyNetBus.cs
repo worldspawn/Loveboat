d94c95da22ef0df24d0d4c13490249e35826ad68
Sam Critchley <critchley.sj@gmail.com>
2012-08-06 12:41:26 +1000

    came back from a git meltdown

--- CQRS.Core/Configuration/EasyNetBus.cs
+++ CQRS.Core/Configuration/EasyNetBus.cs
@@ -1,8 +1,12 @@
 using System;
+using EasyNetQ;
+using EventStore;
+using EventStore.Dispatcher;
+using Magnum.Reflection;
 
 namespace CQRS.Core.Configuration
 {
-    public class EasyNetBus : CQRS.Core.IBus
+    public class EasyNetBus : IBus, IDispatchCommits
     {
         private readonly EasyNetQ.IBus _bus;
 
@@ -11,19 +15,21 @@ namespace CQRS.Core.Configuration
             _bus = bus;
         }
 
+        #region IBus Members
+
         public void PublishEvent(object @event)
         {
-            using (var publishChannel = _bus.OpenPublishChannel())
+            using (IPublishChannel publishChannel = _bus.OpenPublishChannel())
             {
-                publishChannel.Publish(@event);
+                publishChannel.FastInvoke(new[] {@event.GetType()}, "Publish", @event);
             }
         }
 
         public void Send<T>(T command) where T : class, IMessage
         {
-            using (var publishChannel = _bus.OpenPublishChannel())
+            using (IPublishChannel publishChannel = _bus.OpenPublishChannel())
             {
-                publishChannel.Publish(command);
+                publishChannel.FastInvoke(new[] {command.GetType()}, "Publish", command);
             }
         }
 
@@ -31,5 +37,20 @@ namespace CQRS.Core.Configuration
         {
             _bus.Subscribe(Guid.NewGuid().ToString(), handler);
         }
+
+        #endregion
+
+        #region IDispatchCommits Members
+
+        public void Dispose()
+        {
+        }
+
+        void IDispatchCommits.Dispatch(Commit commit)
+        {
+            commit.Events.ForEach(@event => PublishEvent(@event.Body));
+        }
+
+        #endregion
     }
 }
--- CQRS.Core/Configuration/EasyNetQModule.cs
+++ CQRS.Core/Configuration/EasyNetQModule.cs
@@ -16,7 +16,29 @@ namespace CQRS.Core.Configuration
 
         protected override void Load(ContainerBuilder builder)
         {
-            builder.Register(context => RabbitHutch.CreateBus(_connectionString));
+            var uri = new Uri(_connectionString);
+            string username = null, password = null;
+
+            string easyQString, path = uri.AbsolutePath.TrimStart('/');
+            if (path == string.Empty)
+                path = "/";
+
+            if (!string.IsNullOrEmpty(uri.UserInfo))
+            {
+                string[] userInfo = uri.UserInfo.Split(':');
+                username = userInfo[0];
+                password = userInfo[1];
+                easyQString = string.Format("host={0};virtualHost={1};username={2};password={3}", uri.Host,
+                                            path, username, password);
+            }
+            else
+            {
+                easyQString = string.Format("host={0};virtualHost={1}", uri.Host,
+                                            path);
+            }
+
+            builder.Register(context => RabbitHutch.CreateBus(easyQString)).As<EasyNetQ.IBus>().SingleInstance();
+
             builder.RegisterType<EasyNetBus>().AsImplementedInterfaces();
 
             base.Load(builder);
